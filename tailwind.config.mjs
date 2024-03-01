92b1fa3da3312c5285adb0439023d5a93818d51a
Valentina Villegas <valentinavillegasc@hotmail.com>
2024-03-01 15:47:40 -0500

    colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryDark: "#BC5527",
+        primaryLight: "#EC6637",
+        secondaryDark: "#414142",
+        secondaryLight: "#EDDBBE",
+      },
+    },
+  },
+  plugins: [],
+};
